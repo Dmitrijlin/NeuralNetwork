123203fc31bb93709d1d31d07482785da1c1008f
Dmitrij <dmitrij-lin9@rambler.ru>
2016-03-07 21:16:17 +0300

    Lin

---
+++ NeuralNetwork/NeuralNetwork/NetworkHolderMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    class NetworkHolderMethod
+    {
+        int[,] matrix;
+        int x, y;
+        NetworkHolderMethod()
+        {
+            
+        }
+        public void calculate(int[,] matrix)
+        {
+            x = matrix.GetLength(0);
+            y = matrix.GetLength(1);
+        }
+
+    }
+}
